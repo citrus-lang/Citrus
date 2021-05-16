ef9577c3400231faf6852d9fc4953e3f7e1870e6
watcol <potfman@gmail.com>
2021-05-16 16:10:25 +0900

    Add struct "Stream".

--- src/lex.c
+++ src/lex.c
@@ -1,9 +1,49 @@
 #include "citrus.h"
 #include <ctype.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct {
+  char *input;
+  char *cur;
+  size_t col;
+  size_t line;
+} Stream;
+
+Stream *new_stream(char *input) {
+  Stream *stream = calloc(1, sizeof(Stream));
+  stream->input = input;
+  stream->cur = input;
+  stream->col = 1;
+  stream->line = 1;
+  return stream;
+}
+
+int peek(Stream *buf) { return *buf->cur; }
+
+void consume(Stream *buf) {
+  int c = peek(buf);
+  buf->cur += 1;
+  if (c == '\n') {
+    buf->line += 1;
+    buf->col = 1;
+  } else {
+    buf->col += 1;
+  }
+}
+
+void error(Stream *buf, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  fprintf(stderr, "[%lu,%lu] ", buf->line, buf->col);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
 
 typedef enum {
   TK_NUM,
@@ -25,24 +65,37 @@ Token *new_token(TokenKind kind, Token *cur) {
   return tok;
 }
 
-int lex_num(Token **cur, char **buf) {
-  if (isdigit(**buf)) {
-    *cur = new_token(TK_NUM, *cur);
-    (*cur)->val = strtol(*buf, buf, 10);
-    return true;
+int lex_num(Token **cur, Stream **buf) {
+  int c = peek(*buf);
+  int val = 0;
+  if ('1' <= c && c <= '9') {
+    while (true) {
+      c = peek(*buf);
+      if (c < '0' || '9' < c) {
+        break;
+      }
+      consume(*buf);
+      val = val * 10 + (c - '0');
+    }
+  } else if (c == '0') {
+    consume(*buf);
   } else {
     return false;
   }
+
+  *cur = new_token(TK_NUM, *cur);
+  (*cur)->val = val;
+  return true;
 }
 
-Token *lex(char *buf) {
+Token *lex(char *input) {
   Token head;
   head.next = NULL;
   Token *cur = &head;
-  while (*buf) {
+  Stream *buf = new_stream(input);
+  while (peek(buf)) {
     if (!lex_num(&cur, &buf)) {
-      fprintf(stderr, "Cannot lex the input.\n");
-      exit(1);
+      error(buf, "Unnexpected char '%c'.", peek(buf));
     }
   }
   cur = new_token(TK_EOF, cur);
